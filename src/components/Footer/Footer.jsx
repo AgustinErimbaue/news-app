85000b6e8610a0bf06e8d10c228734fa386c4e00
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-25 14:28:07 +0200

    modificacion en scss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.scss'; // Importa el archivo SCSS
+import './Footer.scss';
 
 const Footer = () => {
   return (
